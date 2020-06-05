410c2e5c6a41f634ad8115ce41cd97977d48931a
Babjak Jozef <babjak@domain.local>
2020-06-05 11:17:09 +0200

    CW 01 update

--- CW_01_Love_vs_friendship.js
+++ CW_01_Love_vs_friendship.js
@@ -1,14 +1,5 @@
-var alphabet = 'abcdefghijklmnoprstquvwxyz'.split(''),
-    number = 0;
-function countWord(word) {
-    for(var i=0; i<word.length; i++) {
-        for(var j=0; j<alphabet.length; j++) {
-            if(word[i] === alphabet[j]) {            
-            number += (j+1);
-            }
-        }
-    }
-    console.log("Slovu '" + word + "' odpoveda cislo " + number);
-    return number;
+function wordsToMarks(word){
+    return word.split("")
+                .map(letter => letter.charCodeAt(0) - 96)
+                .reduce((acc, cur) => acc + cur);
 }
-countWord("love");
